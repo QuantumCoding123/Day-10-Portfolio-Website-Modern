61961573ed51b374894806bc698ec8d21ab7ce5e
Quantum Coding <166281221+QuantumCoding123@users.noreply.github.com>
2024-04-26 12:35:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Java Developer", "Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
